df2b44f1f42fe7c766b59819a6db16f48f41ef02
Aleksey <a.goguev@alexgog.ru>
2020-02-29 21:07:20 +0300

    build transport command

--- _build/commands/abstractbuildtransport.class.php
+++ _build/commands/abstractbuildtransport.class.php
@@ -2,10 +2,8 @@
 
 abstract class abstractBuildTransport extends abstractCommand
 {
-    /** @var string|bool */
-    protected $dataFolder = false;
-
-    protected $resolversFolder = false;
+    /** @var string */
+    protected $buildPath = '';
 
     /** @var bool */
     protected $namespace = false;
@@ -16,19 +14,20 @@ abstract class abstractBuildTransport extends abstractCommand
     /** @var bool */
     protected $assetsFiles = false;
 
-    /** @var array|bool */
-    protected $menus = false;
+    /** @var array */
+    protected $data = [];
 
-    /** @var array|bool */
-    protected $events = false;
+    /** @var array */
+    protected $resolvers = [];
 
-    /** @var array|bool */
-    protected $plugins = false;
+    /** @var array */
+    protected $attributes = [];
 
     /** @var modPackageBuilder */
     private $builder;
 
-    private $vehicles = [];
+    /** @var modCategory|null */
+    private $category = NULL;
 
     /**
      * BuildTransport constructor.
@@ -40,9 +39,21 @@ abstract class abstractBuildTransport extends abstractCommand
         parent::__construct($modx, $config);
         $this->modx->loadClass('transport.modPackageBuilder', '', false, true);
         $this->builder = new modPackageBuilder($this->modx);
-        if ($this->dataFolder) {
-            $this->getData();
-        }
+
+        $this->buildPath = dirname(dirname(__FILE__)) . '/' . PKG_NAME_LOWER . '/';
+
+        $corePath = MODX_CORE_PATH . 'components/' . PKG_NAME_LOWER . '/';
+        $this->config = array_merge([
+            'pluginsPath' => $corePath . 'elements/plugins/',
+            'snippetsPath' => $corePath . 'elements/snippets/',
+            'chunksPath' => $corePath . 'elements/chunks/',
+            'changelogPath' => $corePath . 'docs/changelog.txt',
+            'licensePath' => $corePath . 'docs/license.txt',
+            'readmePath' => $corePath . 'docs/readme.txt',
+        ], $config);
+
+        $this->getData();
+        $this->getResolvers();
     }
 
     public function run()
@@ -52,55 +63,71 @@ abstract class abstractBuildTransport extends abstractCommand
         if ($this->namespace) {
             $this->addNamespace();
         }
-
         if ($this->coreFiles) {
-            $this->vehicles['coreFiles'] = $this->addCoreFiles();
+            $this->addCoreFiles();
         }
         if ($this->assetsFiles) {
-            $this->vehicles['assetsFiles'] = $this->addAssetsFiles();
-        }
-        if ($this->menus) {
-            $this->vehicles['menus'] = $this->addMenus();
-        }
-        if ($this->events) {
-            $this->vehicles['events'] = $this->addEvents();
-        }
-        if ($this->plugins) {
-            $this->vehicles['plugins'] = $this->addPlugins();
+            $this->addAssetsFiles();
         }
 
-        if ($this->resolversFolder) {
-            $this->log('DataBase');
-            $this->vehicles['coreFiles']->resolve('php', ['source' => $this->resolversFolder . 'resolve.tables.php']);
+        foreach ($this->data as $key => $items) {
+            $method = 'add' . ucfirst($key);
+            $this->$method($items);
         }
 
-        foreach ($this->vehicles as $vehicle) {
-            if (is_array($vehicle)) {
-                foreach ($vehicle as $subVehicle) {
-                    $this->builder->putVehicle($subVehicle);
-                }
-                continue;
-            }
+        if ($this->category) {
+            $this->addCategory();
+        }
+
+        $this->addAttributes();
+
+        foreach ($this->resolvers as $key => $resolver) {
+            $this->log($key);
+            $vehicle = $this->builder->createVehicle([
+                'source' => $resolver,
+            ], [
+                'vehicle_class' => 'xPDOScriptVehicle',
+            ]);
             $this->builder->putVehicle($vehicle);
         }
-
-
-
 
         $this->builder->pack();
     }
 
-    private function getData()
+    protected function getData()
     {
-        $files = scandir($this->dataFolder);
+        $dataFolder = $this->buildPath . 'data/';
+        if (!is_dir($dataFolder)) {
+            return;
+        }
+        $files = scandir($dataFolder);
         foreach ($files as $file) {
             if (preg_match('/transport.*?\.php$/i', $file)) {
-                $property = str_replace([
+                $key = str_replace([
                     'transport.',
                     '.php',
                 ], '', $file);
                 /** @noinspection PhpIncludeInspection */
-                $this->$property = include $this->dataFolder . $file;
+                $this->data[$key] = include $dataFolder . $file;
+            }
+        }
+    }
+
+    protected function getResolvers()
+    {
+        $resolversFolder = $this->buildPath . 'resolvers/';
+        if (!is_dir($resolversFolder)) {
+            return;
+        }
+        $files = scandir($resolversFolder);
+        foreach ($files as $file) {
+            if (preg_match('/resolve.*?\.php$/i', $file)) {
+                $key = str_replace([
+                    'resolve.',
+                    '.php',
+                ], '', $file);
+                /** @noinspection PhpIncludeInspection */
+                $this->resolvers[$key] = $resolversFolder . $file;
             }
         }
     }
@@ -110,39 +137,31 @@ abstract class abstractBuildTransport extends abstractCommand
         $this->builder->registerNamespace(PKG_NAME_LOWER, false, true, '{core_path}components/' . PKG_NAME_LOWER . '/', '{assets_path}components/' . PKG_NAME_LOWER . '/');
     }
 
-    /**
-     * @return modTransportVehicle
-     */
     private function addCoreFiles()
     {
-        return $this->builder->createVehicle([
+        $vehicle = $this->builder->createVehicle([
             'source' => MODX_CORE_PATH . 'components/' . PKG_NAME_LOWER,
             'target' => "return MODX_CORE_PATH . 'components/';",
         ], [
             'vehicle_class' => 'xPDOFileVehicle',
         ]);
+        $this->builder->putVehicle($vehicle);
     }
 
-    /**
-     * @return modTransportVehicle
-     */
     private function addAssetsFiles()
     {
-        return $this->builder->createVehicle([
+        $vehicle = $this->builder->createVehicle([
             'source' => MODX_ASSETS_PATH . 'components/' . PKG_NAME_LOWER,
             'target' => "return MODX_ASSETS_PATH . 'components/';",
         ], [
             'vehicle_class' => 'xPDOFileVehicle',
         ]);
+        $this->builder->putVehicle($vehicle);
     }
 
-    /**
-     * @return array
-     */
-    private function addMenus()
+    private function addMenus($menus)
     {
-        $menus = [];
-        foreach ($this->menus as $menuData) {
+        foreach ($menus as $menuData) {
             $menu = $this->modx->newObject('modMenu');
             $menu->fromArray(array_merge([
                 'parent' => 'components',
@@ -153,18 +172,13 @@ abstract class abstractBuildTransport extends abstractCommand
                 xPDOTransport::UPDATE_OBJECT => true,
                 xPDOTransport::UNIQUE_KEY => 'text',
             ]);
-            $menus[] = $vehicle;
+            $this->builder->putVehicle($vehicle);
         }
-        return $menus;
     }
 
-    /**
-     * @return array
-     */
-    private function addEvents()
+    private function addEvents($events)
     {
-        $events = [];
-        foreach ($this->events as $eventData) {
+        foreach ($events as $eventData) {
             $event = $this->modx->newObject('modEvent');
             $event->fromArray(array_merge([
                 'service' => 6,
@@ -175,18 +189,19 @@ abstract class abstractBuildTransport extends abstractCommand
                 xPDOTransport::UPDATE_OBJECT => true,
                 xPDOTransport::UNIQUE_KEY => 'name',
             ]);
-            $events[] = $vehicle;
+            $this->builder->putVehicle($vehicle);
         }
-        return $events;
     }
 
-    private function addPlugins()
+    private function addPlugins($plugins)
     {
-        $plugins = [];
-        foreach ($this->plugins as $pluginData) {
+        if (!$this->category) {
+            $this->setCategory();
+        }
+        foreach ($plugins as $pluginData) {
             $plugin = $this->modx->newObject('modPlugin');
             if ($pluginData['static_file']) {
-                $pluginData['plugincode'] = $this->getFileContent($pluginData['static_file']);
+                $pluginData['plugincode'] = $this->getFileContent($this->config['pluginsPath'] . $pluginData['static_file']);
             }
             $plugin->fromArray(array_merge([
                 'id' => 0,
@@ -207,27 +222,102 @@ abstract class abstractBuildTransport extends abstractCommand
                 $plugin->addMany($events);
             }
 
-            $vehicle = $this->builder->createVehicle($plugin, [
-                xPDOTransport::PRESERVE_KEYS => true,
-                xPDOTransport::UPDATE_OBJECT => true,
-                xPDOTransport::UNIQUE_KEY => 'name',
-                xPDOTransport::RELATED_OBJECTS => true,
-                xPDOTransport::RELATED_OBJECT_ATTRIBUTES => [
-                    'PluginEvents' => [
-                        xPDOTransport::PRESERVE_KEYS => true,
-                        xPDOTransport::UPDATE_OBJECT => false,
-                        xPDOTransport::UNIQUE_KEY => [
-                            'pluginid',
-                            'event',
-                        ],
-                    ],
-                ],
-            ]);
-            $plugins[] = $vehicle;
+            $this->category->addMany($plugin);
         }
-        return $plugins;
     }
 
+    private function addSnippets($snippets)
+    {
+        if (!$this->category) {
+            $this->setCategory();
+        }
+        foreach ($snippets as $snippetData) {
+            $snippet = $this->modx->newObject('modSnippet');
+            if ($snippetData['static_file']) {
+                $snippetData['snippet'] = $this->getFileContent($this->config['snippetsPath'] . $snippetData['static_file']);
+            }
+            $snippet->fromArray(array_merge([
+                'id' => 0,
+                'category' => 0,
+            ], $snippetData), '', true, true);
+            $this->category->addMany($snippet);
+        }
+    }
+
+    private function addChunks($chunks)
+    {
+        if (!$this->category) {
+            $this->setCategory();
+        }
+        foreach ($chunks as $chunkData) {
+            $chunk = $this->modx->newObject('modChunk');
+            if ($chunkData['static_file']) {
+                $chunkData['snippet'] = $this->getFileContent($this->config['chunksPath'] . $chunkData['static_file']);
+            }
+            $chunk->fromArray(array_merge([
+                'id' => 0,
+                'category' => 0,
+            ], $chunkData), '', true, true);
+            $this->category->addMany($chunk);
+        }
+    }
+
+    private function setCategory()
+    {
+        $this->category = $this->modx->newObject('modCategory');
+        $this->category->set('id', 1);
+        $this->category->set('category', PKG_NAME);
+    }
+
+    private function addCategory()
+    {
+        $vehicle = $this->builder->createVehicle($this->category, [
+            xPDOTransport::UNIQUE_KEY => 'category',
+            xPDOTransport::PRESERVE_KEYS => false,
+            xPDOTransport::UPDATE_OBJECT => true,
+            xPDOTransport::RELATED_OBJECTS => true,
+            xPDOTransport::RELATED_OBJECT_ATTRIBUTES => [
+                'Snippets' => [
+                    xPDOTransport::PRESERVE_KEYS => false,
+                    xPDOTransport::UPDATE_OBJECT => true,
+                    xPDOTransport::UNIQUE_KEY => 'name',
+                ],
+                'Chunks' => [
+                    xPDOTransport::PRESERVE_KEYS => false,
+                    xPDOTransport::UPDATE_OBJECT => true,
+                    xPDOTransport::UNIQUE_KEY => 'name',
+                ],
+                'Plugins' => [
+                    xPDOTransport::PRESERVE_KEYS => false,
+                    xPDOTransport::UPDATE_OBJECT => true,
+                    xPDOTransport::UNIQUE_KEY => 'name',
+                ],
+                'PluginEvents' => [
+                    xPDOTransport::PRESERVE_KEYS => true,
+                    xPDOTransport::UPDATE_OBJECT => true,
+                    xPDOTransport::UNIQUE_KEY => [
+                        'pluginid',
+                        'event',
+                    ],
+                ],
+            ],
+        ]);
+        $this->builder->putVehicle($vehicle);
+    }
+
+    private function addAttributes()
+    {
+        if (file_exists($this->config['changelogPath'])) {
+            $this->attributes['changelog'] = file_get_contents($this->config['changelogPath']);
+        }
+        if (file_exists($this->config['licensePath'])) {
+            $this->attributes['license'] = file_get_contents($this->config['licensePath']);
+        }
+        if (file_exists($this->config['readmePath'])) {
+            $this->attributes['readme'] = file_get_contents($this->config['readmePath']);
+        }
+        $this->builder->setPackageAttributes($this->attributes);
+    }
 
     /**
      * @param string $filePath
